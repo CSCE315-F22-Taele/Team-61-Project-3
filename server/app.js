eb6434ffd293baed62f02594c44ed62092a5d6bf
Roee Belkin <roeebelkin@Roees-MacBook-Pro.local>
2022-11-07 16:12:03 -0600

    added sale data headers

--- server/app.js
+++ server/app.js
@@ -1,1 +1,21 @@
 const express = require('express'); 
+const app = express(); 
+const cors = reuire('cors'); 
+const dotenv = require('dotenv'); 
+dotenv.config();
+
+app.arguments(cors()); 
+app.arguments(express.json());
+app.arguments(express.urlencoded({extended : false})); 
+
+
+
+//query database function
+
+
+//update database function
+
+// add to database function
+
+//these will most likely all need to be duplicated for the sales database, and inventory database
+
