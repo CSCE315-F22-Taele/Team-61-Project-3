e20bcd23c739c862661d06cd3e1a8865e8990934
justinsingletary <justinsingletary@tamu.edu>
2022-11-16 15:37:38 -0600

    Changed back to localhost

--- client/home/index.js
+++ client/home/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('http://localhost:443/getAll')
+    fetch('https://localhost:5000/getAll')
     .then(response => response.json())
     .then(data => loadHTMLTable(data['data']));
 }); 
