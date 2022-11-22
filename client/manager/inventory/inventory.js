ca36271c56e036af2de1d3070e72bdea73a4a5c0
Brandon Moon <brandonmoon@connect-172-31-56-118.vpn.tamu.edu>
2022-11-22 01:05:13 -0600

    manager sales inventory page now displays the inventory.

--- client/manager/inventory/inventory.js
+++ client/manager/inventory/inventory.js
@@ -1,3 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // fetch('https://project3-7bzcyqo3va-uc.a.run.app/getAll')   //fetch from server on gcloud
+    fetch('http://localhost:5555/getAllInventory') // fetches the getAll function locally
+    .then(response => response.json())
+    .then(data => loadHTMLTable(data['data']));
+}); 
+
+function loadHTMLTable(data) {
+    const table = document.querySelector('table tbody'); 
+    
+    if (data.length === 0) {
+        table.innerHTML = "<tr><td class='no-data' colspan='9'>No Data</td></tr>";
+        return;
+    }
+
+    var tableHTML = "";
+
+    for (var key in data.rows) {
+        tableHTML += "<tr>";
+        for (var keyName in data.rows[key]) {
+            var dataEntry = (data.rows[key])[keyName];
+            tableHTML += `<td>${dataEntry}</td>`;
+        }
+        tableHTML += "</tr>";
+    }
+
+    table.innerHTML = tableHTML;
+}
+
 var btns = document.getElementsByClassName('dropdown_btn'); 
 var button; 
 for (var i = 0; i < btns.length; i++){
