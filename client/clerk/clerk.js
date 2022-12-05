fe980c1e6c53477fe2ac7be970e5414f9f3ff7f0
justinsingletary <justinsingletary@tamu.edu>
2022-12-04 19:32:48 -0600

    commiting changes so i can switch to main

--- client/clerk/clerk.js
+++ client/clerk/clerk.js
@@ -1,5 +1,5 @@
-const link = 'https://project3-7bzcyqo3va-uc.a.run.app'
-//const link = 'http://localhost:5555';
+//const link = 'https://project3-7bzcyqo3va-uc.a.run.app'
+const link = 'http://localhost:5555';
 
 document.addEventListener('DOMContentLoaded', function() {
     fetch(link + '/getEntreeOptions')
