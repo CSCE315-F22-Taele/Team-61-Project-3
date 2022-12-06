cbcfa0d2455b03c738ea01235e89c13fc30eb2ac
Brandon Moon <brandonmoon@connect-172-31-5-236.vpn.tamu.edu>
2022-12-05 23:05:54 -0600

    added variable assignments to button inputs

--- client/manager/newitem/newitem.js
+++ client/manager/newitem/newitem.js
@@ -9,8 +9,40 @@ function change_color(id){
     }
     for (var i = 0; i < all_btns.length; i++){
         if (all_btns[i].id != id){
-            console.log("button with id: ", all_btns[i].id, " should be changing color");
+            // console.log("button with id: ", all_btns[i].id, " should be changing color");
             all_btns[i].style.backgroundColor = '#1995AA'; 
         }
     }
+}
+
+var item_type = "";
+const entreeBtn = document.querySelector('#entreeBtn');
+entreeBtn.addEventListener('click', function() {
+    item_type = 'entree';
+})
+const proteinBtn = document.querySelector('#proteinBtn');
+proteinBtn.addEventListener('click', function() {
+    item_type = 'protein';
+})
+const sideBtn = document.querySelector('#sideBtn');
+sideBtn.addEventListener('click', function() {
+    item_type = 'side';
+})
+const toppingBtn = document.querySelector('#toppingBtn');
+toppingBtn.addEventListener('click', function() {
+    item_type = 'topping';
+})
+
+
+function addItem() {
+    const item_id = document.getElementById('id').value;
+    const quantity = document.getElementById('quantity').value;
+    const cost = document.getElementById('cost').value;
+    const supply = document.getElementById('supply').value;
+    console.log(item_id);
+    console.log(item_type);
+    console.log(quantity);
+    console.log(cost);
+    console.log(supply);
+
 }
